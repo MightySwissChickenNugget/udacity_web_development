2f49762b6b58a70a6f026218004b478b9eb70332
MightySwissChickenNugget <gurkensalat12@gmail.com>
2015-11-17 15:12:21 +0100

    sdf
    sdf

--- blog.py
+++ blog.py
@@ -10,7 +10,7 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
-#testsdfkjhdskljhdsfl
+#testsdfkjhdskljhdsflsdfdssdffd
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
