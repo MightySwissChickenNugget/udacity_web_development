6add2fb20f65caab8e45052f5b429d82bfaffe3a
MightySwissChickenNugget <gurkensalat12@gmail.com>
2015-11-17 17:29:02 +0100

    weiter machen bei login und logout!

--- blog.py
+++ blog.py
@@ -14,6 +14,18 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 secret = "743hkhcjkhzsjkcbnbhksihfw9udhjkJKSHDGKDHKJDFSHBNBKJSDZb"
 
 def render_str(template, **params):
+    t = jinja_env_get_template(template)
+    return t.render(params)
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val). hexidigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|') [0]
+    if secure_val == make_secure_val(val):
+        return val
+
+def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
@@ -24,16 +36,24 @@ class BlogHandler(webapp2.RequestHandler):
     def render_str(self, template, **params):
         return render_str(template, **params)
 
-    def make_secure_val(val):
-        return '%s|%s' % (val, hmac.new(secret, val). hexidigest())
-
-    def check_secure_val(secure_val):
-        val = secure_val.split('|') [0]
-        if secure_val == make_secure_val(val):
-            return val
-
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
+
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request. cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val) #if cookie_val and check_secure_val return cookie_val
+
+    def login(arg):
+        pass
+
+    def logout(arg):
+        pass
 
 def render_post(response, post):
     response.out.write('<b>' + post.subject + '</b><br>')
